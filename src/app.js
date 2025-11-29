59e8b69d68c464adbb5ecf5a7719d79d9e086b38
kernel528 <kernel528@gmail.com>
2025-11-29 14:42:16 -0600

    Updated local postgres image version to v16.11.

--- src/app.js
+++ src/app.js
@@ -17,7 +17,7 @@ const reviewsRouter = require("./reviews/reviews.router");
 // Add routes here
 app.get("/", (req, res) => {
     res.send("Welcome to the We Love Movies API Query Service!<br>" +
-        "Build: v2.3.2, using Node.js, Express, and PostgreSQL v16.10<br>" +
+        "Build: v2.3.3, using Node.js, Express, and PostgreSQL v16.11<br>" +
         "For more information, please visit: https://github.com/kernel528/WeLoveMovies<br>");
 });
 app.use("/movies", moviesRouter);
